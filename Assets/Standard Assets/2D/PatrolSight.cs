cb223b97e92e2f2cc9ff9d9c96238942fdc41bb6
bern422 <bern422@gmail.com>
2015-04-07 22:41:48 -0400

    added line of sight to guard

---
+++ Assets/Standard Assets/2D/PatrolSight.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatrolSight : MonoBehaviour {
+
+	public Transform startSight;
+	public Vector3 endSight;
+	public Patrol patroller;
+	public int foundCount = 0;
+	public bool spotted = false;
+	//public Transform guard;
+	//public int guardSightAmount = 10;
+	
+	// Update is called once per frame
+	void Update () {
+		search();
+		respond();
+	}
+	
+	void search() {
+		int turn = 0;
+		if(patroller.facingRight) {
+			turn = 1;
+		}
+		else turn = -1;
+		endSight = new Vector3((startSight.position.x+(3*turn)), (startSight.position.y), startSight.position.z);//guardSightAmount)*(guard.guardDirection), startSight.position.y, startSight.position.z);
+		Debug.DrawLine(startSight.position, endSight, Color.green); 
+		spotted = Physics2D.Linecast (startSight.position, endSight, 1 << LayerMask.NameToLayer("PlayerCharacterLayer"));
+	}
+	
+	void respond() {
+		if(spotted) {
+			Debug.Log("FOUND THE PLAYER");
+			foundCount++;
+		}
+	}
+}
