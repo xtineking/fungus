d3ad593f8a7ebb54b6d0fd29c8e06cb238c851dc
bern422 <bern422@gmail.com>
2015-04-17 22:23:27 -0400

    oy

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static bool playerSpotted = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate () {
+		if(playerSpotted) {
+			Invoke("killGame", 2);
+		}
+	}
+	
+	void killGame () {
+		Application.LoadLevel("GameOver");
+	}
+}
---
+++ Assets/Scripts/Patroller.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patroller : MonoBehaviour
+{
+	private Animator m_Anim;            // Reference to the player's animator component.
+
+
+	public Transform[] Waypoints;
+	public float Speed;
+	public int curWayPoint;
+	public bool doPatrol = true;
+	public Vector2 Target;
+	public Vector2 MoveDirection;
+	public Vector2 Velocity;
+	public bool facingRight = false;
+
+	void Create() {
+		facingRight = true;
+
+	}
+
+	void Start(){
+		m_Anim = GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		if(curWayPoint < Waypoints.Length)
+		{
+			Target= Waypoints[curWayPoint].position;
+			MoveDirection = Target - (Vector2)transform.position;
+			Velocity = GetComponent<Rigidbody2D>().velocity;
+
+			if(MoveDirection.magnitude < 1)
+			{
+				curWayPoint++;
+				Vector3 theScale = transform.localScale;
+				theScale.x *= -1;
+				transform.localScale = theScale;
+				facingRight = !facingRight;
+			}
+			else
+			{
+				Velocity = MoveDirection.normalized * Speed;
+			}
+		}
+		else
+		{
+			if(doPatrol)
+			{
+				curWayPoint=0;
+			}
+			else
+			{
+				Velocity = Vector2.zero;
+			}
+		}
+
+		GetComponent<Rigidbody2D>().velocity = Velocity;
+	}
+
+
+}
